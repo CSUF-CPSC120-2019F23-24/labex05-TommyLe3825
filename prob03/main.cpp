7bc7d3ade7830aff28d30d20490b259dab14ea4c
TommyLe3825 <54484078+TommyLe3825@users.noreply.github.com>
2019-10-14 22:44:31 -0700

    Updated Constants and Spacing

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -6,20 +6,20 @@
 
 int main ()
 {
-  const int max = 40; //the maximum for length/base of rectangle
-  const int height = 3; // the constant height
+  const int MAX = 40; //the maximum for length/base of rectangle
+  const int HEIGHT = 3; // the constant height
   double length; //random length
   unsigned hope = time (0); //time is very random
   srand (hope);
 
-  length=rand() % max+1; //it creates a random length
+  length = rand() % MAX + 1; //it creates a random length
 
   std::string sign;
   sign.assign(length,'#'); //makes as many '#' according to the value of the length
 
-  std::cout<< sign << std::endl; //length of rectangle is constant for all
-  std::cout<< sign << std::endl; // the height is 3 so there is 3 cout
-  std::cout<< sign << std::endl;
+  std::cout << sign << std::endl; //length of rectangle is constant for all
+  std::cout << sign << std::endl; // the height is 3 so there is 3 cout
+  std::cout << sign << std::endl;
 
   return 0;
 }
